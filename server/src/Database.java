1388875fc3f5e3b3d563507145dfd3cc2b8632cd
MOHAX239 <vova.filatov@gmail.com>
2013-03-05 23:01:06 +0400

    add dbscript

---
+++ server/src/Database.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Владимир
+ * Date: 05.03.13
+ * Time: 22:12
+ * To change this template use File | Settings | File Templates.
+ */
+
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Database
+{
+
+}
