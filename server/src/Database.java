0ded34c6e12063c841021a25c39b073eeeb441a9
MOHAX239 <vova.filatov@gmail.com>
2013-03-14 23:57:30 +0400

    add parser xml sportObjects, competitions, loginCountries

--- server/src/Database.java
+++ server/src/Database.java
@@ -11,7 +11,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class Database
 {
@@ -21,11 +20,10 @@ public class Database
         Class.forName("org.gjt.mm.mysql.Driver").newInstance();
 
         // String url = "jdbc:mysql://localhost/commining";
-        String url = "jdbc:mysql://localhost/youtube";
+        String url = "jdbc:mysql://localhost/olimpis";
         String username = "root";
         String password = "pass";
         return DriverManager.getConnection(url, username, password);
-
     }
 
     public Database() throws Exception{
@@ -34,4 +32,129 @@ public class Database
     public void closeConnection() throws SQLException {
         conn.close();
     }
+
+    public int getIdTypeObject(String object){
+        PreparedStatement stmt = null;
+        int id = 0;
+        try {
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "SELECT id  FROM sportobject_type WHERE object_type=?;");
+            stmt.setString(1, object);
+            ResultSet rs = stmt.executeQuery();
+            if (!rs.first()) {
+                stmt = (PreparedStatement) conn.prepareStatement(
+                     "INSERT INTO sportobject_type  Values (NULL,?)");
+                stmt.setString(1, object);
+                stmt.executeUpdate();
+            }
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "SELECT id  FROM sportobject_type WHERE object_type=?;");
+            stmt.setString(1,object);
+            rs = stmt.executeQuery();
+            rs.next();
+            id =  rs.getInt("id");
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return id;
+    }
+
+    public String getCountry(String login, String password){
+        PreparedStatement stmt = null;
+        String res = "";
+        try {
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "SELECT country_name  FROM country WHERE login=? and hash_password=?;");
+            stmt.setString(1, login);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                res = rs.getString("country_name");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return res;
+    }
+
+    public void  insertInSportObject(String title, String type, String city, int capacity){
+        PreparedStatement stmt = null;
+        int id = 0;
+        try {
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "SELECT id  FROM sport_object WHERE title=?;");
+            stmt.setString(1, title);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.first()) {
+                stmt.close();
+                return;
+            }
+            int typeObject = getIdTypeObject(type);
+          stmt = (PreparedStatement) conn.prepareStatement(
+                    "INSERT INTO sport_object  Values (NULL,?,?,?,?)");
+           stmt.setString(1,title);
+           stmt.setInt(2,typeObject);
+           stmt.setInt(3,capacity);
+           stmt.setString(4,city);
+           stmt.executeUpdate();
+           stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public void  insertInCompetition(String title, String typeObject, int sexParticipants, int duration){
+        PreparedStatement stmt = null;
+        int id = 0;
+        try {
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "SELECT id  FROM competition WHERE competition_name=?;");
+            stmt.setString(1, title);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.first()) {
+                stmt.close();
+                return;
+            }
+            int idObject = getIdTypeObject(typeObject);
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "INSERT INTO competition  Values (NULL,?,?,?,?)");
+            stmt.setString(1,title);
+            stmt.setInt(2,idObject);
+            stmt.setInt(3,sexParticipants);
+            stmt.setInt(4,duration);
+            stmt.executeUpdate();
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public void  insertInCountries(String name, String login,String pass){
+        PreparedStatement stmt = null;
+        int id = 0;
+        try {
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "SELECT id  FROM country WHERE country_name=?;");
+            stmt.setString(1, name);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.first()) {
+                stmt.close();
+                return;
+            }
+            stmt = (PreparedStatement) conn.prepareStatement(
+                    "INSERT INTO country  Values (NULL,?,?,?)");
+            stmt.setString(1,name);
+            stmt.setString(2,login);
+            stmt.setString(3,pass);
+            stmt.executeUpdate();
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
 }
--- server/src/XMLutils.java
+++ server/src/XMLutils.java
@@ -1,11 +1,17 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.StringWriter;
 
 /**
@@ -32,10 +38,88 @@ public class XMLutils {
 		throw new DomToStringTranslationException();
 	}
 
+    public static void parserSportObjects(String filePath){
+        try {
+            Database db = new Database();
+            File fXmlFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("object");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String title =eElement.getAttribute("title");
+                    String type = eElement.getElementsByTagName("type").item(0).getTextContent();
+                    String city = eElement.getElementsByTagName("city").item(0).getTextContent();
+                    Integer capacity = Integer.parseInt(eElement.getElementsByTagName("capacity").item(0).getTextContent());
+                    db.insertInSportObject(title,type,city,capacity);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void parserContries(String filePath){
+        try {
+            Database db = new Database();
+            File fXmlFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("country");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String name =eElement.getAttribute("name");
+                    String login = eElement.getElementsByTagName("login").item(0).getTextContent();
+                    String password = eElement.getElementsByTagName("password").item(0).getTextContent();
+                    db.insertInCountries(name,login, password);
+                }
+            }
+            db.closeConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void parserCompetitions(String filePath){
+        try {
+            Database db = new Database();
+            File fXmlFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("competition");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String title =eElement.getAttribute("title");
+                    String type = eElement.getElementsByTagName("type_object").item(0).getTextContent();
+                    int sex_participants = Integer.parseInt(eElement.getElementsByTagName("sex_participants").item(0).getTextContent());
+                    Integer duration = Integer.parseInt(eElement.getElementsByTagName("duration").item(0).getTextContent());
+                    db.insertInCompetition(title,type,sex_participants,duration);
+                }
+            }
+            db.closeConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 	public static class DomToStringTranslationException extends Exception {
 	}
 
-	// Just for masking constructor
+	// Just for masking construhttps://github.com/MOHAX239/olympISctor
 	private XMLutils() {
 	}
 }
