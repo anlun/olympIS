58941ce8222371ec0d219bd73187f467a046748b
Podkopaev Anton <podkoav239@gmail.com>
2013-03-25 21:02:39 +0400

    Sex becomes class instead of enum.

--- shared/src/beans/Sex.java
+++ shared/src/beans/Sex.java
@@ -1,10 +1,29 @@
 package beans;
 
 /**
- * Enum represents the sex of sportmen in competitions.
+ * Class represents the sex of sportsmen in competitions.
  * @author Podkopaev Anton
  */
 
-public enum Sex {
-	Male, Female, Undefined
+public class Sex {
+	public Sex() {
+	}
+
+	public Sex(int sex) {
+		this.sex = sex;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int newSex) {
+		sex = newSex;
+	}
+
+	public static final int male      = 1;
+	public static final int female    = 0;
+	public static final int undefined = 2;
+
+	private int sex;
 }
