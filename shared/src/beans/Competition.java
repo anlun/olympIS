bcf98ba70918be23cbd4695df8cfd41c1c41466a
MOHAX239 <vova.filatov@gmail.com>
2013-04-02 22:00:15 +0400

    changed arrayList required sport objects on the idTypeRequiredSportObject

--- shared/src/beans/Competition.java
+++ shared/src/beans/Competition.java
@@ -7,47 +7,47 @@ package beans;
  * @author Podkopaev Anton
  */
 public class Competition {
-    public Competition(int id, int duration, Sex athleteSex, int idTypeRequiredSportObject) {
-        this.id = id;
-        this.duration = duration;
-        this.athleteSex = athleteSex;
-        this.idTypeRequiredSportObject = idTypeRequiredSportObject;
-    }
+	public Competition(int id, int duration, Sex athleteSex, int idTypeRequiredSportObject) {
+		this.id = id;
+		this.duration = duration;
+		this.athleteSex = athleteSex;
+		this.idTypeRequiredSportObject = idTypeRequiredSportObject;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public int getDuration() {
-        return duration;
-    }
+	public int getDuration() {
+		return duration;
+	}
 
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
 
-    public Sex getAthleteSex() {
-        return athleteSex;
-    }
+	public Sex getAthleteSex() {
+		return athleteSex;
+	}
 
-    public void setAthleteSex(Sex athleteSex) {
-        this.athleteSex = athleteSex;
-    }
+	public void setAthleteSex(Sex athleteSex) {
+		this.athleteSex = athleteSex;
+	}
 
-    public int getidTypeRequiredSportObject() {
-        return idTypeRequiredSportObject;
-    }
+	public int getidTypeRequiredSportObject() {
+		return idTypeRequiredSportObject;
+	}
 
-    public void setidTypeRequiredSportObject(int idTypeRequiredSportObject) {
-        this.idTypeRequiredSportObject = idTypeRequiredSportObject;
-    }
+	public void setidTypeRequiredSportObject(int idTypeRequiredSportObject) {
+		this.idTypeRequiredSportObject = idTypeRequiredSportObject;
+	}
 
-    private int id;
-    private int duration;
-    private Sex athleteSex;
-    private int idTypeRequiredSportObject;
+	private int id;
+	private int duration;
+	private Sex athleteSex;
+	private int idTypeRequiredSportObject;
 }
