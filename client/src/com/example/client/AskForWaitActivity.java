b6dc9b98f2a7cbfa34ebfa5e46528b72f06cd590
danyaberezun <danya.berezun@gmail.com>
2013-04-04 02:37:30 +0400

    AskForWaitActivity теперь не может быть закрыто пользователем.

--- client/src/com/example/client/AskForWaitActivity.java
+++ client/src/com/example/client/AskForWaitActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 
 public class AskForWaitActivity extends Activity {
 
@@ -12,16 +13,11 @@ public class AskForWaitActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.wait_activity);
 		Log.d("DAN", "ask wait started");
-		// Устанавливаем текст.
-		// ((TextView) findViewById(R.id.waitActivityTextView)).setText(this.getIntent().getStringExtra("textToSet"));
 	}
 
-	/**
-	 * Closes this activity.
-	 */
-	public void finishAskForWaitActivity() {
-		Intent intent1 = new Intent(this, CountryGUI.class);
-		startActivity(intent1);
-		this.finish();
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		Log.d("DAN", "переназначили кнопку, чтобы не закрывалось Activity");
+		return true;
 	}
+
 }
