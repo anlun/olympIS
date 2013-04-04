bfc09123e194e2da77efa65bb94e3f852419fb5c
Podkopaev Anton <podkoav239@gmail.com>
2013-04-04 12:16:26 +0400

    Continue working

--- server/src/FilterResponseCreator.java
+++ server/src/FilterResponseCreator.java
@@ -1,3 +1,10 @@
+import beans.DayList;
+import beans.FilterList;
+import utils.Utils;
+
+import java.beans.XMLDecoder;
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
 
 public class FilterResponseCreator extends ResponseCreator {
 	public FilterResponseCreator(String filterListXML) {
@@ -5,8 +12,23 @@ public class FilterResponseCreator extends ResponseCreator {
 	}
 
 	public String createResponse() {
-		//TODO: сделать response
-		return "";
+		try {
+			XMLDecoder decoder = new XMLDecoder(
+					new ByteArrayInputStream(filterListXML.getBytes("UTF-8"))
+			);
+			FilterList filterList = (FilterList) decoder.readObject();
+			if (filterList == null) {
+				return Utils.beanToString(new DayList());
+			}
+
+			//TODO: чтение из базы DayList по filterList
+			return Utils.beanToString(new DayList());
+
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("Unsupported encoding error!");
+		}
+
+		return Utils.beanToString(new DayList());
 	}
 
 	private String filterListXML;
