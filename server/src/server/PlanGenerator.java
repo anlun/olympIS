a8f740baae6ef1e6c99d704d90ddbbce7c786dea
Roman Oderov <roman.oderov@gmail.com>
2013-04-06 18:50:56 +0400

    Added db connectivity

--- server/src/server/PlanGenerator.java
+++ server/src/server/PlanGenerator.java
@@ -1,12 +1,8 @@
 package server;
 
-import beans.Sex;
-
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-//TODO: сейчас все работает только локально (без БД). Сделать с БД!
 
 /**
  * Class server.PlanGenerator for generating competition plan.
@@ -16,7 +12,7 @@ public class PlanGenerator {
 
     public PlanGenerator() {
         try{
-            ArrayList<Competition> result = new ArrayList<Competition>();
+            /*ArrayList<Competition> result = new ArrayList<Competition>();
             result.add(new Competition(1,3,new Sex(Sex.undefined),1));
             result.add(new Competition(2,4,new Sex(Sex.undefined),1));
             result.add(new Competition(3,3,new Sex(Sex.undefined),1));
@@ -30,22 +26,22 @@ public class PlanGenerator {
             result.add(new Competition(11,8,new Sex(Sex.undefined),3));
             result.add(new Competition(12,3,new Sex(Sex.undefined),1));
             result.add(new Competition(13,5,new Sex(Sex.undefined),1));
+            */
+
+            this.db = Database.createDatabase();
+
+            this.nonPlannedCompetitions = db.competitions();//this.nonPlannedCompetitions = result;
+            this.allCompetitionCount = this.nonPlannedCompetitions.size();
+            this.sportObjectCount = db.sportObjectNumber();//this.sportObjectCount = 4;
 
             //spObjs by type:
-            spObjByType = new ArrayList[4];
+            /*spObjByType = new ArrayList[4];
             spObjByType[0] = null;
             spObjByType[1] = new ArrayList<Integer>(Arrays.asList(1,2));
             spObjByType[2] = new ArrayList<Integer>(Arrays.asList(3));
             spObjByType[3] = new ArrayList<Integer>(Arrays.asList(4));
-            //Database db = Database.createDatabase();
-            //ArrayList<Integer>[] spObjByType = db.allSportObjectsByType();
-
-            this.nonPlannedCompetitions = result;
-            //Database db = Database.createDatabase();
-            //this.nonPlannedCompetitions = db.competitions();
-            this.allCompetitionCount = this.nonPlannedCompetitions.size();
-            //this.sportObjectCount = db.sportObjectNumber();
-            this.sportObjectCount = 4;
+            */
+            this.spObjByType = db.allSportObjectsByType();
 
             this.plan = new int[this.sportObjectCount + 1][this.allCompetitionCount*DAY_LENGTH + 1];
             this.plannedCompetitions = new ArrayList<Competition>();
@@ -58,15 +54,26 @@ public class PlanGenerator {
                 this.currDay[i] = 1;
                 this.currHour[i] = 1;
             }
-            /*try {
-                db.closeConnection();
-            } catch (SQLException e) {
-                System.err.print("\nCan't close database connection!");
-                e.printStackTrace();
-            } */
+            //get all collisions between competitions into array:
+            this.athleteCollisions = new boolean[allCompetitionCount + 1][allCompetitionCount + 1];
+            for (int i = 1; i <= allCompetitionCount; i++) {//zero-row and zero-column are empty: there's no competition with ID = 0
+                this.athleteCollisions[i][i] = false;//no collision with itself
+                for (int j = i + 1; j <= allCompetitionCount; j++) {
+                    this.athleteCollisions[i][j] = db.athleteCollision(i,j);
+                    this.athleteCollisions[j][i] = this.athleteCollisions[i][j];//Array is symmetrical about the main diagonal.
+                }
+            }
         }
         catch (Exception e){
             System.err.print("\nCan't create DB connection!");
+        }
+
+        //close db connection:
+        try {
+            this.db.closeConnection();
+        } catch (SQLException e) {
+            System.err.print("\nCan't close database connection!");
+            e.printStackTrace();
         }
     }
 //======================================
@@ -199,7 +206,7 @@ public class PlanGenerator {
         for (int d = 0; d < competition.getDuration(); d++) {
             for (int sO = 1; sO <= sportObjectCount; sO++) {
                 //if it's not itself and if there's a collision return false;
-                if ((sO != sportObject) && (athleteCollision(competition.getId(),plan[sO][currHour[sportObject] + d]))) {
+                if ((sO != sportObject) && (athleteCollisions[competition.getId()][plan[sO][currHour[sportObject] + d]])) {
                     return false;
                 }
             }
@@ -240,29 +247,6 @@ public class PlanGenerator {
     }
 
     /**
-     * Function checking if there're athlete collisions between two competitions
-     * i.e. same athlete participates in two concurrent competitions.
-     * @param cmptnID1 first competition's ID
-     * @param cmptnID2 second competition's ID
-     * @return TRUE if there's a collision. FALSE - on the contrary
-     */
-    private boolean athleteCollision(int cmptnID1, int cmptnID2) {
-        //TODO: implement it with DB
-        if (
-            (cmptnID1 == 1 && cmptnID2 == 6)||(cmptnID1 == 1 && cmptnID2 == 6)||
-            (cmptnID1 == 2 && cmptnID2 == 5)||(cmptnID1 == 5 && cmptnID2 == 2)||
-            (cmptnID1 == 2 && cmptnID2 == 12)||(cmptnID1 == 12 && cmptnID2 == 2)||
-            (cmptnID1 == 11 && cmptnID2 == 12)||(cmptnID1 == 12 && cmptnID2 == 11)||
-            (cmptnID1 == 13 && cmptnID2 == 8)||(cmptnID1 == 8 && cmptnID2 == 13)||
-            (cmptnID1 == 3 && cmptnID2 == 4)||(cmptnID1 == 4 && cmptnID2 == 3)||
-            (cmptnID1 == 3 && cmptnID2 == 8)||(cmptnID1 == 8 && cmptnID2 == 3)
-                ) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
      * Export renewed competition info back to the server.Database
      * Now it writes plan in the console
      */
@@ -288,12 +272,17 @@ public class PlanGenerator {
      */
     public static void main(String[] args) {
         PlanGenerator planGen = new PlanGenerator();
+
         boolean success = planGen.generatePlan();
+
+        //export calculated data and
         if (success) {
             planGen.exportPlanToDatabase();
         } else {
             System.out.print("ERROR! Plan generation failed. There's no suitable plan.");
         }
+
+        //exit:
         try {
             System.out.print("\n\n=========\nPRESS ENTER!");
             System.in.read();
@@ -307,12 +296,11 @@ public class PlanGenerator {
     private Database db; //database entity
 
     private ArrayList<Competition> nonPlannedCompetitions; // competitions not planned yet
+    //private Competition[] competitionIDs;    //can be used to set bijection between Competitions and their IDs.
     private int allCompetitionCount;   //count of existing competitions
     private int sportObjectCount;   //count of existing sportObjects
 
     private ArrayList<Integer>[] spObjByType;   //type <-> spObjects
-
-    //private Competition[] competitionIDs;    //used to set bijection between Competitions and their IDs.
 
     private int[][] plan;           //main structure for the plan
     private ArrayList<Competition> plannedCompetitions;    // competitions added to plan
@@ -321,6 +309,34 @@ public class PlanGenerator {
     private int[] hoursLeft; // Hours left from current time to next day // maybe set this field for easier way to write code
     private int[] currDay;   // current day of the Championship/Games at each sportObject
 
+    private boolean[][] athleteCollisions;   //array [allCompetitionCount][allCompetitionCount] to store collisions from db
     private final static int DAY_LENGTH = 8;
-    private final static int MAX_DAYS = 2;//21;
+    private final static int MAX_DAYS = 21;
 }
+
+
+
+
+/** -- FOR TEST -- -- FOR TEST -- -- FOR TEST -- -- FOR TEST -- -- FOR TEST --
+ * ---------------------------------------------------------------------------
+ * Function checking if there're athlete collisions between two competitions
+ * i.e. same athlete participates in two concurrent competitions.
+ * @param cmptnID1 first competition's ID
+ * @param cmptnID2 second competition's ID
+ * @return TRUE if there's a collision. FALSE - on the contrary
+ */
+    /*private boolean athleteCollision(int cmptnID1, int cmptnID2) {
+        if (
+            (cmptnID1 == 1 && cmptnID2 == 6)||(cmptnID1 == 1 && cmptnID2 == 6)||
+            (cmptnID1 == 2 && cmptnID2 == 5)||(cmptnID1 == 5 && cmptnID2 == 2)||
+            (cmptnID1 == 2 && cmptnID2 == 12)||(cmptnID1 == 12 && cmptnID2 == 2)||
+            (cmptnID1 == 11 && cmptnID2 == 12)||(cmptnID1 == 12 && cmptnID2 == 11)||
+            (cmptnID1 == 13 && cmptnID2 == 8)||(cmptnID1 == 8 && cmptnID2 == 13)||
+            (cmptnID1 == 3 && cmptnID2 == 4)||(cmptnID1 == 4 && cmptnID2 == 3)||
+            (cmptnID1 == 3 && cmptnID2 == 8)||(cmptnID1 == 8 && cmptnID2 == 3)
+                ) {
+            return true;
+        }
+        return false;
+    }
+    */
